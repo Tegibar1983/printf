4bfe4aeab10a3e4429940d38ddb9a83b2bffe204
Tegibar1983 <tbtf1991@gmail.com>
2023-08-21 23:27:21 -0700

    Updated

--- functions.c
+++ functions.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /*** Print charactor files ***/
 
 /**
@@ -17,9 +18,10 @@ int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
 char x = va_arg(types, int);
-
 return (handle_write_char(x, buffer, flags, width, precision, size));
 }
+
+
 
 /*** Print string files ***/
 /**
@@ -33,30 +35,27 @@ return (handle_write_char(x, buffer, flags, width, precision, size));
  * Return: List of charactors to be printed
  */
 
+
 int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
 int length = 0, r;
 char *s = va_arg(types, char *);
-
 UNUSED(buffer);
 UNUSED(flags);
 UNUSED(width);
 UNUSED(precision);
 UNUSED(size);
-if (str == NULL)
+if (s == NULL)
 {
 s = "(null)";
 if (precision >= 6)
-s = "      ";
+s = " ";
 }
-
 while (s[length] != '\0')
 length++;
-
 if (precision >= 0 && precision < length)
 length = precision;
-
 if (width > length)
 {
 if (flags & F_MINUS)
@@ -74,11 +73,12 @@ write(1, &s[0], length);
 return (width);
 }
 }
-
 return (write(1, s, length));
 }
 
+
 /***Function that prints perecent sign***/
+
 /**
  * print_percent - Function that prints percent sign
  * @types: count of arguments
@@ -89,6 +89,7 @@ return (write(1, s, length));
  * @size: Parameter that specifies size
  * Return: List of charactors to be printed
  */
+
 
 int print_percent(va_list types, char buffer[],
 int flags, int width, int precision, int size)
@@ -120,36 +121,30 @@ int flags, int width, int precision, int size)
 {
 int r = BUFF_SIZE - 2;
 int is_negative = 0;
-long int n = va_arg(types, long int);
+long int m = va_arg(types, long int);
 unsigned long int num;
-
 m = convert_size_number(m, size);
-
 if (m == 0)
 buffer[r--] = '0';
-
 buffer[BUFF_SIZE - 1] = '\0';
 num = (unsigned long int)m;
-
 if (m < 0)
 {
 num = (unsigned long int)((-1) * m);
 is_negative = 1;
 }
-
 while (num > 0)
 {
 buffer[r--] = (num % 10) + '0';
 num /= 10;
 }
-
 r++;
-
 return (write_number(is_negative, r, buffer, flags, width, precision, size));
 }
 
 
 /*** Function that prints Binary ***/
+
 /**
  * print_binary - Function that prints a binary num
  * @types: count of arguments in the function
@@ -161,21 +156,23 @@ return (write_number(is_negative, r, buffer, flags, width, precision, size));
  * Return: List of charactors printed
  */
 
+
 int print_binary(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
 unsigned int m, n, r, sum;
 unsigned int z[32];
 int count;
-
 UNUSED(buffer);
 UNUSED(flags);
 UNUSED(width);
 UNUSED(precision);
 UNUSED(size);
-
 m = va_arg(types, unsigned int);
-n = 2147483648; /* (2 ^ 31) */
+n = 2147483648;
+
+/*** (2 ^ 31) ***/
+
 z[0] = m / n;
 for (r = 1; r < 32; r++)
 {
@@ -186,10 +183,9 @@ for (r = 0, sum = 0, count = 0; r < 32; r++)
 {
 sum += z[r];
 if (sum || r == 31)
-		{
-char a = '0' + z[r];
-
-write(1, &a, 1);
+{
+char z = '0' + z[r];
+write(1, &z, 1);
 count++;
 }
 }
